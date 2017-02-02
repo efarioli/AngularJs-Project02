1d318fc153d366685164701a0310949c92f36c5a
zeke farioli <efarioli@gmail.com>
2017-02-02 23:18:11 +0000

    add functionality to edit classified

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -33,14 +33,35 @@
       			$scope.classifieds.push(classified);
       			$scope.classified = {};
       			$scope.closeSidebar();
-      			$mdToast.show(
-      				$mdToast.simple()
-      				  .content("Classified Save!!!")
-      				  .position('top, right')
-      				  .hideDelay(3000)
-
-      			);
+      			showToast("Classified saved!!!");
+      			
       		}
+      	}
+
+      	$scope.editClassified = function(classified) {
+
+      		$scope.editing = true;
+      		$scope.openSidebar();
+      		$scope.classified = classified;
+      	}
+
+      	$scope.saveEdit = function() {
+
+      		$scope.editing = false;
+      		$scope.classified = {};
+      		$scope.closeSidebar();
+      		showToast("Edit saved!!!");
+      	}
+
+      	function showToast(message) {
+      		$mdToast.show(
+      			$mdToast.simple()
+      			  .content(message)
+      			  .position('top, right')
+      			  .hideDelay(3000)
+
+      		);
+
       	}
       
 
